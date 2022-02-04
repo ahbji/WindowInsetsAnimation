75376191d55ed816d9914eeb1467fc260c94a843
andreahbji <andrewpetrovic@gmail.com>
2022-02-04 21:16:28 +0800

    初始化 Modern UI 框架

---
+++ app/src/main/java/com/codingnight/android/insetsanimation/ConversationFragment.kt
@@ -0,0 +1,29 @@
+package com.codingnight.android.insetsanimation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ConversationFragment : Fragment() {
+
+    private lateinit var conversationRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conversation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        conversationRecyclerView = view.findViewById(R.id.conversation_recyclerview)
+
+        conversationRecyclerView.adapter = ConversationAdapter()
+    }
+
+}
