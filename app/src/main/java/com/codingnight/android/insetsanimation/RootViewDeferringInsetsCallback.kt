15d79f609ce1fa32a15f610fb0ca89ba87f60040
andreahbji <andrewpetrovic@gmail.com>
2022-02-04 23:10:57 +0800

    对 insets animations 做出回应

--- app/src/main/java/com/codingnight/android/insetsanimation/RootViewDeferringInsetsCallback.kt
+++ app/src/main/java/com/codingnight/android/insetsanimation/RootViewDeferringInsetsCallback.kt
@@ -2,12 +2,15 @@ package com.codingnight.android.insetsanimation
 
 import android.view.View
 import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RootViewDeferringInsetsCallback(
     val persistentInsetTypes: Int,
     val deferredInsetTypes: Int
-): OnApplyWindowInsetsListener {
+): OnApplyWindowInsetsListener,
+    WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_CONTINUE_ON_SUBTREE){
 
     init {
         require(persistentInsetTypes and deferredInsetTypes == 0) {
@@ -16,12 +19,19 @@ class RootViewDeferringInsetsCallback(
         }
     }
 
+    private var view: View? = null
+    private var lastWindowInsets: WindowInsetsCompat? = null
+
     private var deferredInsets = false
 
     override fun onApplyWindowInsets(
         v: View,
         windowInsets: WindowInsetsCompat
     ): WindowInsetsCompat {
+        //
+        view = v
+        lastWindowInsets = windowInsets
+
         val types = when {
             // 只应用 systemBars() insets
             deferredInsets -> persistentInsetTypes
@@ -37,4 +47,36 @@ class RootViewDeferringInsetsCallback(
         return WindowInsetsCompat.CONSUMED
     }
 
+    override fun onPrepare(animation: WindowInsetsAnimationCompat) {
+        if (animation.typeMask and deferredInsetTypes != 0) {
+            // We defer the WindowInsetsCompat.Type.ime() insets if the IME is currently not visible.
+            // This results in only the WindowInsetsCompat.Type.systemBars() being applied, allowing
+            // the scrolling view to remain at it's larger size.
+            deferredInsets = true
+        }
+    }
+
+    override fun onProgress(
+        insets: WindowInsetsCompat,
+        runningAnims: List<WindowInsetsAnimationCompat>
+    ): WindowInsetsCompat {
+        // This is a no-op. We don't actually want to handle any WindowInsetsAnimations
+        return insets
+    }
+
+    override fun onEnd(animation: WindowInsetsAnimationCompat) {
+        if (deferredInsets && (animation.typeMask and deferredInsetTypes) != 0) {
+            // If we deferred the IME insets and an IME animation has finished, we need to reset
+            // the flag
+            deferredInsets = false
+
+            // And finally dispatch the deferred insets to the view now.
+            // Ideally we would just call view.requestApplyInsets() and let the normal dispatch
+            // cycle happen, but this happens too late resulting in a visual flicker.
+            // Instead we manually dispatch the most recent WindowInsets to the view.
+            if (lastWindowInsets != null && view != null) {
+                ViewCompat.dispatchApplyWindowInsets(view!!, lastWindowInsets!!)
+            }
+        }
+    }
 }
